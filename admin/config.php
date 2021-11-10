188d2edefb725b517626367d67ffd51246d725e2
iftakharul-islam <iftakharulislam881@gmail.com>
2021-11-10 08:09:36 -0800

    Create User and Showing

---
+++ admin/config.php
@@ -0,0 +1,7 @@
+<?php
+$conn = mysqli_connect("localhost","root","","php_news")
+or
+die("Connection failed : ".mysqli_connect_error());
+
+$host = "http://localhost/News-Project/news-template/";
+
--- admin/users.php
+++ admin/users.php
@@ -1,65 +1,52 @@
 <?php include "header.php"; ?>
-  <div id="admin-content">
-      <div class="container">
-          <div class="row">
-              <div class="col-md-10">
-                  <h1 class="admin-heading">All Users</h1>
-              </div>
-              <div class="col-md-2">
-                  <a class="add-new" href="add-user.php">add user</a>
-              </div>
-              <div class="col-md-12">
-                  <table class="content-table">
-                      <thead>
-                          <th>S.No.</th>
-                          <th>Full Name</th>
-                          <th>User Name</th>
-                          <th>Role</th>
-                          <th>Edit</th>
-                          <th>Delete</th>
-                      </thead>
-                      <tbody>
-                          <tr>
-                              <td class='id'>1</td>
-                              <td>Ram Sharma</td>
-                              <td>ram</td>
-                              <td>admin</td>
-                              <td class='edit'><a href='update-user.php'><i class='fa fa-edit'></i></a></td>
-                              <td class='delete'><a href='delete-user.php'><i class='fa fa-trash-o'></i></a></td>
-                          </tr>
-                          <tr>
-                              <td class='id'>2</td>
-                              <td>Shyam Kumar</td>
-                              <td>shyam</td>
-                              <td>normal</td>
-                              <td class='edit'><a href='update-user.php'><i class='fa fa-edit'></i></a></td>
-                              <td class='delete'><a href='delete-user.php'><i class='fa fa-trash-o'></i></a></td>
-                          </tr>
-                          <tr>
-                              <td class='id'>3</td>
-                              <td>Ramesh Kumar</td>
-                              <td>ramesh</td>
-                              <td>admin</td>
-                              <td class='edit'><a href='update-user.php'><i class='fa fa-edit'></i></a></td>
-                              <td class='delete'><a href='delete-user.php'><i class='fa fa-trash-o'></i></a></td>
-                          </tr>
-                          <tr>
-                              <td class='id'>4</td>
-                              <td>Satish Sharma</td>
-                              <td>satish</td>
-                              <td>admin</td>
-                              <td class='edit'><a href='update-user.php'><i class='fa fa-edit'></i></a></td>
-                              <td class='delete'><a href='delete-user.php'><i class='fa fa-trash-o'></i></a></td>
-                          </tr>
-                      </tbody>
-                  </table>
-                  <ul class='pagination admin-pagination'>
-                      <li class="active"><a>1</a></li>
-                      <li><a>2</a></li>
-                      <li><a>3</a></li>
-                  </ul>
-              </div>
-          </div>
-      </div>
-  </div>
+<div id="admin-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10">
+                <h1 class="admin-heading">All Users</h1>
+            </div>
+            <div class="col-md-2">
+                <a class="add-new" href="add-user.php">add user</a>
+            </div>
+            <div class="col-md-12">
+                <table class="content-table">
+                    <thead>
+                        <th>S.No.</th>
+                        <th>Full Name</th>
+                        <th>User Name</th>
+                        <th>Role</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </thead>
+                    <tbody>
+                        <?php
+                        include "config.php";
+                        $sql = "SELECT * FROM user ORDER BY user_id DESC";
+                        $result = mysqli_query($conn, $sql) or die("Query Failed");
+                        if (mysqli_num_rows($result) > 0) :
+                            foreach ($result as $user) :
+                        ?>
+                                <tr>
+                                    <td class='id'><?php echo $user['user_id'] ?></td>
+                                    <td><?php echo $user['first_name']." ".$user['last_name'] ?></td>
+                                    <td><?php echo $user['username'] ?></td>
+                                    <td><?php echo $user['role'] == 1?"Admin":"Normal"; ?></td>
+                                    <td class='edit'><a href='update-user.php?edit=<?php echo $user['user_id'] ?>'><i class='fa fa-edit'></i></a></td>
+                                    <td class='delete'><a href="delete-user.php?delete=<?php echo $user['user_id'] ?>"><i class='fa fa-trash-o'></i></a></td>
+                                </tr>
+                        <?php
+                            endforeach;
+                        endif;
+                        ?>
+                    </tbody>
+                </table>
+                <ul class='pagination admin-pagination'>
+                    <li class="active"><a>1</a></li>
+                    <li><a>2</a></li>
+                    <li><a>3</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
 <?php include "header.php"; ?>
